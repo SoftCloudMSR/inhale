1da5490e10489fabcadd5925cc9401543f4d9c16
rotarsebastian <rotar.seby1@gmail.com>
2025-03-11 21:15:38 +0100

    ✨ Add proper package exports

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import { Home } from '@rotarsebastian/inhale';
+
+export default function TestPage() {
+  return (
+    <div>
+      <h1>Testing @rotarsebastian/inhale package</h1>
+      <Home />
+    </div>
+  );
+}
